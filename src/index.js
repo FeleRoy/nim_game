0fb9f6396cf552b7ee06938c425b4ffb1312374e
Baratov Viktor <vitya.baratov@gmail.com>
2024-10-26 14:19:51 +0300

    сделал формы

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import { openModal, closeModal, closeByEsc, closeByOverlay } from "../scripts/mo
 
 const popupStart = document.querySelector('.popup_type-start');
 const buttonStart = document.querySelector('.button_type-start');
+const buttonNext = popupStart.querySelector('.popup__button');
+const popupHowMany = document.querySelector('.popup_type-how-many');
 
 const buttonStartClose = popupStart.querySelector('.popup__button-close');
+const buttonHowManyClose = popupHowMany.querySelector('.popup__button-close');
 
-buttonStart.addEventListener('click', ()=> {
+buttonStart.addEventListener('click', (evt)=> {
     openModal(popupStart);
 });
+
+buttonNext.addEventListener('click', (evt)=>{
+    evt.preventDefault();
+    closeModal(popupStart);
+    openModal(popupHowMany);
+})
+
 buttonStartClose.addEventListener('click', ()=>{
     closeModal(popupStart);
+})
+buttonHowManyClose.addEventListener('click', ()=> {
+    closeModal(popupHowMany);
 })
